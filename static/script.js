151266b5f97347cb00f121d3773bcf95dada2815
Artoreus <frtoreus@mail.ru>
2024-05-07 20:43:43 +0300

    gitignore added - интеграция с учетом исключений 2

--- static/script.js
+++
@@ -1,7 +0,0 @@
-function showOrHideModal() {
-    document.querySelector('.modal').classList.toggle('is-active')
-}
-
-function toggleReplyForm(id) {
-    document.getElementById(id).classList.toggle('is-hidden')
-}
